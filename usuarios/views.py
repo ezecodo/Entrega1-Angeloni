7b0663cc9abefa0c0b9350684db8025b16fdc7c0
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-10-17 15:48:17 -0600

    Formulario de registro de Usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm  # Importa desde aquí
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views.generic import TemplateView
+
 
 class UsuarioRegistroView(CreateView):
     form_class = UserCreationForm
-    template_name = 'registro/registro_form.html'
+    template_name = 'usuarios/registro_form.html'
     success_url = reverse_lazy('registro_exitoso')
 
-class RegistroExitosoView(TemplateView):
-    template_name = 'registro/registro_exitoso.html'
+
