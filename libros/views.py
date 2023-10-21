41d0ee1acc364c65703605660c27bccebc9175eb
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-10-20 18:57:01 -0600

    Muestra Hola Usuario a la derecha

--- libros/views.py
+++ libros/views.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.shortcuts import render, redirect
 from django.shortcuts import render, redirect
-from .forms import IngresarLibroForm
+from .forms import LibroForm
 from django.shortcuts import render
 from .models import Libro
 from django.contrib.auth.decorators import login_required
@@ -14,16 +14,16 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def ingresar_libro(request):
     if request.method == 'POST':
-        form = IngresarLibroForm(request.POST)
+        form = LibroForm(request.POST)
         if form.is_valid():
-            # Verificar si el libro ya existe en la biblioteca
+            
             titulo = form.cleaned_data['titulo']
             autor = form.cleaned_data['autor']
             if not Libro.objects.filter(titulo=titulo, autor=autor).exists():
                 form.save()
-                return redirect('lista_libros')  # Redirigir a la lista de libros
+                return redirect('lista_libros') 
             else:
-                # Mostrar mensaje de error si el libro ya existe
+                
                 form.add_error('titulo', 'Este libro ya existe en la biblioteca.')
     else:
         form = IngresarLibroForm()
@@ -33,6 +33,6 @@ def ingresar_libro(request):
 
 
 def lista_libros(request):
-    libros = Libro.objects.all().order_by('titulo')  # Ordenar por título
+    libros = Libro.objects.all().order_by('titulo') 
     return render(request, 'libros/lista_libros.html', {'libros': libros})
 
