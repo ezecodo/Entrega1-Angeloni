fa32f43cb2107feeaf93278bab62f04b0bbce3e6
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-10-20 22:17:52 -0600

    Sube imagen de Libro

--- libros/forms.py
+++ libros/forms.py
@@ -4,4 +4,5 @@ from .models import Libro
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
-        fields = ['titulo', 'autor', 'genero', 'descripcion']
+        fields = ['titulo', 'autor', 'genero', 'descripcion', 'imagen']
+       
--- libros/models.py
+++ libros/models.py
@@ -5,6 +5,7 @@ class Libro(models.Model):
     autor = models.CharField(max_length=100)
     genero = models.CharField(max_length=50)
     descripcion = models.TextField(blank=True, null=True) 
+    imagen = models.ImageField(upload_to='libros/', blank=True, null=True)
 
     def __str__(self):
         return self.titulo
