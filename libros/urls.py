3f551dbedf15d23f0e3ce1f3c6bad03ae804f005
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-10-20 21:11:32 -0600

    Funcion Listar libros anadida

--- libros/urls.py
+++ libros/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('ingresar_libro/', views.ingresar_libro, name='ingresar_libro'),
+    path('lista_libros/', views.lista_libros, name='lista_libros'),
    
     
 ]
