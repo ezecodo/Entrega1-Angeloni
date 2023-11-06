6b885beca4315640347eb325c94f8c2d4a366df9
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-11-06 14:46:21 +0100

    Acerca de nosotros creado

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import about
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('about/', about, name='about'),
 ]
