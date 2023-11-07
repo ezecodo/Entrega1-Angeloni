db5dd386ea8027ce56f0a075fb89a8aebcee96ff
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-11-07 21:28:17 +0100

    Carrusel de libros en pagina de inicio

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from libros.models import Libro 
 
 def homepage(request):
-    return render(request, 'inicio/homepage.html')
+    libros_recientes = Libro.objects.all()[:10] 
+    return render(request, 'inicio/homepage.html', {'libros_recientes': libros_recientes})
 
 def about(request):
     return render(request, 'inicio/about.html')
