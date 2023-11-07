e12a03f3c1b3f99ded76a9d1f05901419072a167
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2023-11-07 21:57:36 +0100

    Carrusel Slide de libros listo

--- inicio/views.py
+++ inicio/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from libros.models import Libro 
 
 def homepage(request):
-    libros_recientes = Libro.objects.all()[:10] 
-    return render(request, 'inicio/homepage.html', {'libros_recientes': libros_recientes})
+    libros_recientes = list(Libro.objects.all()[:10])  
+    libros_recientes_grupo = [libros_recientes[i:i+3] for i in range(0, len(libros_recientes), 3)]
+    return render(request, 'inicio/homepage.html', {'libros_recientes': libros_recientes_grupo})
+
 
 def about(request):
     return render(request, 'inicio/about.html')
